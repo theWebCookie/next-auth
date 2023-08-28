f8cdd7299296177a401d8f0c4f07a0bf1b24e6c7
theWebCookie <jabwojtek@wp.pl>
2023-08-28 17:39:11 +0200

    use ServerSession

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,24 +1,31 @@
 import { verifyPassword } from '@/lib/auth';
 import connectToDatabase from '@/lib/db';
-import NextAuth from 'next-auth/next';
-import CredentialsProvider from 'next-auth/providers/credentials';
-export default NextAuth({
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+
+export const authOptions = {
+  secret: 'thequickbrownfox',
   session: {
     jwt: true,
   },
   providers: [
-    CredentialsProvider({
+    Credentials({
       async authorize(credentials) {
         const client = await connectToDatabase();
+
         const usersCollection = client.db().collection('users');
 
-        const user = await usersCollection.findOne({ email: credentials.email });
+        const user = await usersCollection.findOne({
+          email: credentials.email,
+        });
+
         if (!user) {
           client.close();
           throw new Error('No user found!');
         }
 
         const isValid = await verifyPassword(credentials.password, user.password);
+
         if (!isValid) {
           client.close();
           throw new Error('Could not log you in!');
@@ -29,4 +36,6 @@ export default NextAuth({
       },
     }),
   ],
-});
+};
+
+export default NextAuth(authOptions);
