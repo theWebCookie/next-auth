77e3951b804ef9ffcc0955aea360c966237b0411
theWebCookie <jabwojtek@wp.pl>
2023-08-28 15:10:02 +0200

    add credentials auth provider

--- pages/api/auth/auth.js
+++
@@ -1,6 +0,0 @@
-const { hash } = require('bcryptjs');
-
-export async function hashPassword(password) {
-  const hashedPassword = await hash(password, 12);
-  return hashedPassword;
-}
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,5 +1,5 @@
+import { hashPassword } from '@/lib/auth';
 import connectToDatabase from '@/lib/db';
-import { hashPassword } from './auth';
 
 async function handler(req, res) {
   if (req.method !== 'POST') {
